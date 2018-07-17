57e3993d77e4f5abaf19d23ed76561432a80085b
Patricia Carter <patricia4194@gmail.com>
2018-07-17 14:30:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,16 @@ const codes = [
   "a"
 ];
 
-function init(e) {
+let index = 0;
+
+function init() {
   const key = e.key
+  
+  if(key === codes[index]) {
+    index++;
+    
+    if (index === codes.length) {
+      alert("You win!")
+    }
+  }
 }
